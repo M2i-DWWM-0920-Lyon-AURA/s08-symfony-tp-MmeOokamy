c4d2f7f16024a3ec3e33a4f2eaf4b598ed061682
Manue <manue.ggt@gmail.com>
2020-11-05 15:06:03 +0100

    3.1

--- src/Controller/RecetteListeController.php
+++ src/Controller/RecetteListeController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Meal;
+use App\Entity\Ingredient;
+use App\Entity\Instruction;
+use App\Repository\MealRepository;
+use App\Repository\IngredientRepository;
+use App\Repository\InstructionRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+     * @Route("/", name="recette_")
+     */
 class RecetteListeController extends AbstractController
 {
+    protected MealRepository $repository;
+    
+    public function __construct(MealRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+    
     /**
-     * @Route("/", name="recette_liste")
+     * @Route("", name="list")
      */
     public function index(): Response
     {
+            $recettes = $this->repository->findAll();
+
         return $this->render('recette_liste/index.html.twig', [
-            'controller_name' => 'RecetteListeController',
+            'recettes' => $recettes,
         ]);
     }
 }
