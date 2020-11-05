4fe2ad32d1dbef537578806fbdeeb09a986e88b8
Manue <manue.ggt@gmail.com>
2020-11-05 09:23:14 +0100

    add bdd meal-instruction-ingredient

---
+++ src/Entity/Instruction.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InstructionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=InstructionRepository::class)
+ */
+class Instruction
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $instructionRank;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getInstructionRank(): ?int
+    {
+        return $this->instructionRank;
+    }
+
+    public function setInstructionRank(int $instructionRank): self
+    {
+        $this->instructionRank = $instructionRank;
+
+        return $this;
+    }
+}
